0977b90ec4103e349ce8208e131dc2921743b299
nilguncelik <nilgun.celik@mekansalisler.com>
2015-07-31 23:12:53 +0300

    docs removed

--- docs/scripts/compileAllSpecRunners.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `find * -type f | grep -i spec | grep -i "SpecRunner.html$" | grep -v "third_party"`; 
-do
-    compiledJs=`echo $i | sed -e 's/SpecRunner.html/compiled.js/'`
-    echo -e "========= COMPILING $compiledJs =========" ; 
-    scripts/compileForSpecRunner.sh $compiledJs 2> /dev/null; 
-    echo -e "========= FINISHED $? =========\n"; 
-done
--- docs/scripts/deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#usage : scripts/deps.sh
-
-third_party/goog/closure/bin/build/depswriter.py --root_with_prefix='tart/ ../../../../tart/' --output_file='tart/deps.js'
